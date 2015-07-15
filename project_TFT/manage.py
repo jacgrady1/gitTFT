e99df51a0c15923616291d3483d8e83f5fb84661
Vic <Sentinel@Weiqis-MacBook-Pro-2.local>
2015-07-14 23:17:14 -0700

    config manage.py to stop generating pyc file

--- project_TFT/manage.py
+++ project_TFT/manage.py
@@ -6,5 +6,7 @@ if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project_TFT.settings")
 
     from django.core.management import execute_from_command_line
-
+    
+    sys.dont_write_bytecode = True
+    
     execute_from_command_line(sys.argv)
