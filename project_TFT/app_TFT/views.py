f8d4554448676dcec233b75626455134bf3b26f6
jacgrady1 <lgxjackyzyx@gmail.com>
2015-07-17 18:36:32 -0700

    change in question and teacher in course

--- project_TFT/app_TFT/views.py
+++ project_TFT/app_TFT/views.py
@@ -23,7 +23,20 @@ def course(request,id):
     context={}
     course = Course.objects.get(id=id)
     questions = Question.objects.filter(course__id=id)
+    teachers = Teacher.objects.filter(course=course)
+    lecturer=[]
+    coaches=[]
+    for teacher in teachers:
+        if teacher.role=='Lecturer':
+            lecturer.append(teacher)
+        else:
+            coaches.append(teacher)
+    print "teacher:",lecturer
+    print "coach:",coaches
     context={'course':course,
-             'questions':questions}
+             'questions':questions,
+             'lecturers':lecturer,
+             'coaches':coaches
+             }
     return render(request,"course.html",context)
     
